15a6df3963690bd0ab1ff16457f8e0a04061e7d2
Rafael Ferreira Monteiro <rafaelferreirax3@gmail.com>
2025-03-27 23:12:15 -0300

    feat: implement CreateCategory use cases with corresponding unit and integration tests

---
+++ src/category/application/__tests__/integration/create-category.use-case.int-spec.ts
@@ -0,0 +1,39 @@
+import {CreateCategoryUseCase} from "../../create-category.use-case";
+import {CategorySequelizeRepository} from "../../../infra/db/sequelize/category-sequelize.repository";
+import {setupSequelize} from "../../../../shared/infra/testing/helpers";
+import {CategoryModel} from "../../../infra/db/sequelize/category.model";
+import {Uuid} from "../../../../shared/domain/value-objects/uuid.vo";
+
+describe('CreateCategoryUseCase Integration Tests', () => {
+    let useCase: CreateCategoryUseCase;
+    let repository: CategorySequelizeRepository;
+
+    setupSequelize({ models: [CategoryModel] });
+
+    beforeEach(() => {
+        repository = new CategorySequelizeRepository(CategoryModel);
+        useCase = new CreateCategoryUseCase(repository);
+    });
+
+    test('should create and insert a category', async () => {
+        let output = await useCase.execute({ name: 'Movie' });
+        let entity = await repository.findById(new Uuid(output.id));
+        expect(output).toStrictEqual({
+            id: entity.category_id.id,
+            name: 'Movie',
+            description: null,
+            is_active: true,
+            created_at: entity.created_at,
+        });
+
+        output = await useCase.execute({ name: 'Movie', description: 'some description', is_active: false });
+        entity = await repository.findById(new Uuid(output.id));
+        expect(output).toStrictEqual({
+            id: entity.category_id.id,
+            name: 'Movie',
+            description: 'some description',
+            is_active: false,
+            created_at: entity.created_at,
+        });
+    });
+});
---
+++ src/category/application/__tests__/unit/create-category.use-case.spec.ts
@@ -0,0 +1,38 @@
+import {CreateCategoryUseCase} from "../../create-category.use-case";
+import {CategoryInMemoryRepository} from "../../../infra/db/in-memory/category-in-memory.repository";
+
+class CategoryRepository {
+}
+
+describe('CreateCategoryUseCase Unit Tests', () => {
+    let useCase: CreateCategoryUseCase;
+    let categoryRepository: CategoryInMemoryRepository;
+
+    beforeEach(() => {
+        categoryRepository = new CategoryInMemoryRepository();
+        useCase = new CreateCategoryUseCase(categoryRepository);
+    });
+
+    test('should create and insert a category', async () => {
+        const spyInsert = jest.spyOn(categoryRepository, 'insert');
+        let output = await useCase.execute({ name: 'Movie' });
+        expect(spyInsert).toHaveBeenCalledTimes(1);
+        expect(output).toStrictEqual({
+            id: categoryRepository.items[0].category_id.id,
+            name: 'Movie',
+            description: null,
+            is_active: true,
+            created_at: categoryRepository.items[0].created_at,
+        });
+
+        output = await useCase.execute({ name: 'Movie', description: 'description', is_active: false });
+        expect(spyInsert).toHaveBeenCalledTimes(2);
+        expect(output).toStrictEqual({
+            id: categoryRepository.items[1].category_id.id,
+            name: 'Movie',
+            description: 'description',
+            is_active: false,
+            created_at: categoryRepository.items[1].created_at,
+        });
+    });
+});
