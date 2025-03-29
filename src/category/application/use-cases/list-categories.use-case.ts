9a0bbbb3758481a35c4a6255e6c3313f1e88b32a
Rafael Ferreira Monteiro <rafaelferreirax3@gmail.com>
2025-03-28 22:42:32 -0300

    feat: implement ListCategories use case with pagination output

---
+++ src/category/application/use-cases/list-categories.use-case.ts
@@ -0,0 +1,26 @@
+import {IUseCase} from "../../../shared/application/use-case.interface";
+import {CategorySearchParams, CategorySearchResult, ICategoryRepository} from "../../domain/category.repository";
+import {PaginationOutput, PaginationOutputMapper} from "../../../shared/application/pagination-output";
+import {CategoryOutput, CategoryOutputMapper} from "./common/category-output";
+
+export type ListCategoriesInput = CategorySearchParams;
+
+export type ListCategoriesOutput = PaginationOutput<CategoryOutput>;
+
+export class ListCategoriesUseCase implements IUseCase<ListCategoriesInput, ListCategoriesOutput> {
+    constructor(private categoryRepository: ICategoryRepository) {}
+
+    async execute(input: ListCategoriesInput): Promise<ListCategoriesOutput> {
+        const params = new CategorySearchParams(input);
+
+        const searchResult = await this.categoryRepository.search(params);
+
+        return this.toOutput(searchResult);
+    }
+
+    private toOutput(searchResult: CategorySearchResult): ListCategoriesOutput {
+        const { items: _items } = searchResult;
+        const items = _items.map((item) => CategoryOutputMapper.toOutput(item));
+        return PaginationOutputMapper.toOutput(items, searchResult);
+    }
+}
