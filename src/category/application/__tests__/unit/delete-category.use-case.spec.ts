0c58a6b539d79bfd718026f8bcfaafdf9d66ab19
Rafael Ferreira Monteiro <rafaelferreirax3@gmail.com>
2025-03-28 21:22:00 -0300

    feat: implement DeleteCategory use case with integration and unit tests

---
+++ src/category/application/__tests__/unit/delete-category.use-case.spec.ts
@@ -0,0 +1,37 @@
+import {CategoryInMemoryRepository} from "../../../infra/db/in-memory/category-in-memory.repository";
+import {DeleteCategoryUseCase} from "../../delete-category.use-case";
+import {InvalidUuidError, Uuid} from "../../../../shared/domain/value-objects/uuid.vo";
+import {NotFoundError} from "../../../../shared/domain/errors/not-found.error";
+import {Category} from "../../../domain/category.entity";
+
+describe('DeleteCategoryUseCase Unit Tests', () => {
+    let useCase: DeleteCategoryUseCase;
+    let categoryRepository: CategoryInMemoryRepository;
+
+    beforeEach(() => {
+        categoryRepository = new CategoryInMemoryRepository();
+        useCase = new DeleteCategoryUseCase(categoryRepository);
+    });
+
+    test('should throw error when category not found', async () => {
+        await expect(() => useCase.execute({ id: 'invalid_id' }))
+            .rejects
+            .toThrow(new InvalidUuidError());
+
+        const uuid = new Uuid();
+
+        await expect(() => useCase.execute({ id: uuid.id }))
+            .rejects
+            .toThrow(new NotFoundError(uuid.id, Category));
+    });
+
+    test('should delete a category', async () => {
+        const category = Category.fake().aCategory().build();
+        categoryRepository.items = [category];
+        const spyDelete = jest.spyOn(categoryRepository, 'delete');
+        const output = await useCase.execute({ id: category.category_id.id });
+        expect(spyDelete).toHaveBeenCalledTimes(1);
+        expect(output).toBeUndefined();
+        expect(categoryRepository.items).toHaveLength(0);
+    });
+});
---
+++ src/category/application/delete-category.use-case.ts
@@ -0,0 +1,18 @@
+import {IUseCase} from "../../shared/application/use-case.interface";
+import {ICategoryRepository} from "../domain/category.repository";
+import {Uuid} from "../../shared/domain/value-objects/uuid.vo";
+
+export type DeleteCategoryInput = {
+    id: string;
+}
+
+export type DeleteCategoryOutput = void;
+
+export class DeleteCategoryUseCase implements IUseCase<DeleteCategoryInput, DeleteCategoryOutput> {
+    constructor(private readonly categoryRepository: ICategoryRepository) {}
+
+    async execute(input: DeleteCategoryInput): Promise<DeleteCategoryOutput> {
+        const uuid = new Uuid(input.id);
+        await this.categoryRepository.delete(uuid);
+    }
+}
