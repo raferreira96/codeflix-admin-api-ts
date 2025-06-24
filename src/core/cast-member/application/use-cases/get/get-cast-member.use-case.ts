508b169c4a57671ee69d609931a2a9981dd1edb5
Rafael Ferreira Monteiro <rafaelferreirax3@gmail.com>
2025-06-24 00:09:28 -0300

    feat: add cast member get use case

---
+++ src/core/cast-member/application/use-cases/get/get-cast-member.use-case.ts
@@ -0,0 +1,30 @@
+import {IUseCase} from "@core/shared/application/use-case.interface";
+import {ICastMemberRepository} from "@core/cast-member/domain/cast-member.repository";
+import {CastMember, CastMemberId} from "@core/cast-member/domain/cast-member.aggregate";
+import {NotFoundError} from "@core/shared/domain/errors/not-found.error";
+import {
+    CastMemberOutput,
+    CastMemberOutputMapper
+} from "@core/cast-member/application/use-cases/common/cast-member.output";
+
+export class GetCastMemberUseCase
+    implements IUseCase<GetCastMemberInput, GetCastMemberOutput>
+{
+    constructor(private castMemberRepo: ICastMemberRepository) {}
+
+    async execute(input: GetCastMemberInput): Promise<GetCastMemberOutput> {
+        const castMemberId = new CastMemberId(input.id);
+        const castMember = await this.castMemberRepo.findById(castMemberId);
+        if (!castMember) {
+            throw new NotFoundError(input.id, CastMember);
+        }
+
+        return CastMemberOutputMapper.toOutput(castMember);
+    }
+}
+
+export type GetCastMemberInput = {
+    id: string;
+};
+
+export type GetCastMemberOutput = CastMemberOutput;
